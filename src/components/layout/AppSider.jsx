b6c581076571a1fd3ca8950bf38f611a1abb475d
Sodnom322 <Sodnomtyksheev@Gmail.com>
2024-03-01 22:06:21 +0900

    init app

---
+++ src/components/layout/AppSider.jsx
@@ -0,0 +1,61 @@
+import React, { useContext } from "react";
+import { Card, Layout, Statistic, List, Typography, Tag } from "antd";
+import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
+
+import { capitalize } from "../../utils";
+import CryptoContext from "../../context/crypto-context";
+const siderStyle = {
+  padding: "1rem",
+};
+
+const AppSider = () => {
+  const { loading, assets } = useContext(CryptoContext);
+
+  return (
+    <Layout.Sider width="25%" style={siderStyle}>
+      {assets.map((asset) => (
+        <Card key={asset.id} style={{ marginBottom: "1rem" }}>
+          <Statistic
+            title={capitalize(asset.id)}
+            value={asset.totalAmout}
+            precision={2}
+            valueStyle={{ color: asset.grow ? "#3f8600" : "#cf1322" }}
+            prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+            suffix="$"
+          />
+          <List
+            size="small"
+            dataSource={[
+              {
+                title: "Total Profit",
+                value: asset.totalProfit,
+                withTag: true,
+              },
+              { title: "Assets Amout", value: asset.amount, isPlane: true },
+            ]}
+            renderItem={(item) => (
+              <List.Item>
+                <span>{item.title}</span>
+                <span>
+                  {item.withTag && (
+                    <Tag color={asset.grow ? "green" : "red"}>
+                      {asset.growPercent}%
+                    </Tag>
+                  )}
+                  {item.isPlane && item.value}
+                  {!item.isPlane && (
+                    <Typography.Text type={asset.grow ? "success" : "danger"}>
+                      {item.value.toFixed(2)}$
+                    </Typography.Text>
+                  )}
+                </span>
+              </List.Item>
+            )}
+          />
+        </Card>
+      ))}
+    </Layout.Sider>
+  );
+};
+
+export default AppSider;
